b6657ce238f21ada06440bdec65605e83051f40b
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-10 10:54:28 +0200

    cardData

--- src/components/Card1.js
+++ src/components/Card1.js
@@ -1,10 +1,15 @@
 export default function Card1() {
+  const cardData = {
+    town: 'New York',
+    date: 'Fri 27 Nov 2016',
+    img: '/img/newyork.jpg',
+  };
   return (
     <li className='band-card'>
-      <img className='card__img' src='/img/newyork.jpg' alt='paris' />
+      <img className='card__img' src={cardData.img} alt={cardData.town} />
       <div className='card__info'>
-        <h3 className='card__title'>New York</h3>
-        <p className='card__date'>Fri 27 Nov 2016</p>
+        <h3 className='card__title'>{cardData.town}</h3>
+        <p className='card__date'>{cardData.date}</p>
         <p className='cart__text'>
           Praesent tincidunt sed tellus ut rutrum sed vitae justo.
         </p>
--- src/components/Card2.js
+++ src/components/Card2.js
@@ -1,10 +1,15 @@
 export default function Card2() {
+  const cardData = {
+    town: 'Parios',
+    date: 'Fri 28 Nov 2016',
+    img: '/img/paris.jpg',
+  };
   return (
     <li className='band-card'>
-      <img className='card__img' src='/img/paris.jpg' alt='paris' />
+      <img className='card__img' src={cardData.img} alt={cardData.town} />
       <div className='card__info'>
-        <h3 className='card__title'>Paris</h3>
-        <p className='card__date'>Fri 28 Nov 2016</p>
+        <h3 className='card__title'>{cardData.town}</h3>
+        <p className='card__date'>{cardData.date}</p>
         <p className='cart__text'>
           Praesent tincidunt sed tellus ut rutrum sed vitae justo.
         </p>
