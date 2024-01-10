f2160b413d10b08d3386f5b850b1df311d6cbefd
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2024-01-10 10:11:40 +0200

    section start

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Our first CRA
+
+![](assets/2024-01-10-10-01-53.png)
--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
-      <div className='container'>
-        <h1>Hello from React</h1>
-      </div>
+      <section className='bands-section'>
+        <div className='container'>
+          <h2 className='section__title'>Tour dates</h2>
+          <p className='section__subtitle'>Remember to book your tickets!</p>
+
+          <ul className='unlisted grid'>
+            <li className='band-card'>
+              <img className='card__img' src='/img/newyork.jpg' alt='paris' />
+              <div className='card__info'>
+                <h3 className='card__title'>New York</h3>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
     </div>
   );
 }
